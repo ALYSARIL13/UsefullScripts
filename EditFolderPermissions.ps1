4e25b397ba33f6a0b5a987624b986ff356be43b6
ALYSARIL13 <39834053+ALYSARIL13@users.noreply.github.com>
2018-06-01 10:01:08 -0600

    Add files via upload

---
+++ EditFolderPermissions.ps1
@@ -0,0 +1,78 @@
+﻿	#=====>Setting up variable#
+	$folder = "<FolderPath>"
+	 
+	#=====>Change the owner to "BUILTIN/Administrators, in order to grant permissions#
+	TAKEOWN /A /F $folder
+	 
+	#=====>Removing old folder permissions#
+	$folderAcl = (Get-ACL $folder).Access
+	While($folderAcl -ne $null){
+	    $acl = Get-Acl $folder
+	    $acl.Access | ForEach{$acl.RemoveAccessRule($_)}
+	    $acl | ForEach{$acl.SetAccessRuleProtection($true,$true)}
+	    icacls $folder /reset
+	    Set-Acl $folder $acl
+	    $folderAcl = (Get-ACL $folder).Access
+	}
+	#=====>Setting up new folder Permissions
+	$acl = Get-Acl $folder
+	$rule = New-Object  system.security.accesscontrol.filesystemaccessrule("Administrators","FullControl","Allow") 
+	$acl.SetAccessRule($rule)
+	icacls $folder /reset
+	Set-Acl $folder $acl
+	 
+	$acl = Get-Acl $folder
+	$rule = New-Object  system.security.accesscontrol.filesystemaccessrule("Everyone","Write,Read,Synchronize","Allow") 
+	$acl.SetAccessRule($rule) 
+	icacls $folder /reset
+	Set-Acl $folder $acl
+	 
+	#=====>Giving back permissions to "NT AUTHORITY\SYSTEM"#
+	icacls $folder /setowner "NT AUTHORITY\SYSTEM"
+	 
+	#=====>Removing old file permissions#
+	$objectsAcl = Get-ChildItem $folder
+	ForEach ($object in $objectsAcl){
+	    TAKEOWN /A /F $object.FullName
+	    $acl = Get-Acl $object.FullName
+	    While($acl -ne $null){
+	        $acl = Get-Acl $object.FullName
+	        $acl.Access | ForEach{$acl.RemoveAccessRule($_)}
+	        $acl | ForEach{$acl.SetAccessRuleProtection($true,$true)}
+	        icacls $object.FullName /reset
+	        Set-Acl $object.FullName $acl
+	        $acl = (Get-ACL $object.FullName).Access
+	    }
+	    #=====>Setting up new file Permissions#
+	    $acl = Get-Acl $object.FullName
+	    $rule = New-Object  system.security.accesscontrol.filesystemaccessrule("SYSTEM","FullControl","Allow") 
+	    $acl.SetAccessRule($rule)
+	    icacls $object.FullName /reset
+	    Set-Acl $object.FullName $acl
+	 
+	    $acl = Get-Acl $object.FullName
+	    $rule = New-Object  system.security.accesscontrol.filesystemaccessrule("Administrators","FullControl","Allow") 
+	    $acl.SetAccessRule($rule)
+	    icacls $object.FullName /reset 
+	    Set-Acl $object.FullName $acl
+	 
+	    $acl = Get-Acl $object.FullName
+	    $rule = New-Object  system.security.accesscontrol.filesystemaccessrule("NETWORK SERVICE","Write,Read,Synchronize","Allow") 
+	    $acl.SetAccessRule($rule)
+	    icacls $object.FullName /reset 
+	    Set-Acl $object.FullName $acl
+	 
+	    icacls $object.FullName /setowner "NT AUTHORITY\SYSTEM"
+	}
+	 
+	#=====>Showing results of execution
+	$Output = @()
+	$Output = $Output + (Get-ACL $Folder)
+	ForEach($object in $objectsAcl){
+	    $Output = $Output + (Get-ACL $object.FullName)
+	}
+	$Output | Out-GridView
+	 
+	#=====>Restart TermService#
+	Get-Service -ServiceName TermService | Stop-Service –Force
+	Get-Service -ServiceName TermService | Start-Service
---
+++ WindowsUserManagement.ps1
@@ -0,0 +1,43 @@
+﻿#Setup the variables#
+$UserName = "username"
+$Password = "P@ssw0rd1234" #--> 12 or more characters #
+ 
+#Create the user account
+$ComputerName = $env:COMPUTERNAME
+[ADSI]$ADSIcomp = "WinNT://$ComputerName"
+$NewUser = $ADSIcomp.Create("User","$UserName")
+ 
+#Set password on the account 
+Try{
+    $NewUser.SetPassword("$Password")
+    $NewUser.SetInfo()
+    Write-Host "The user '$UserName' was created successfully"
+}Catch [System.Management.Automation.MethodInvocationException]{
+    Write-Warning $Error[0].Exception.Message.Split('"')[-2]
+    Try{
+        $ExistingUser = $ADSIcomp.Children|?{$_.Path -eq $NewUser.Path}
+        $ExistingUser.SetPassword("$Password")
+        $ExistingUser.SetInfo()
+        Write-Host "The password for user '$UserName' was changed successfully."
+    }Catch [System.Management.Automation.RuntimeException]{
+        Write-Warning $Error[0]
+    }
+}
+ 
+#Add the account to the Groups: "Administartors" & "Remote Desktop Users"
+$AdminGroup = "Administrators"
+$RDPgroup = "Remote Desktop Users"
+Try{
+    [ADSI]$UsersGroup = "WinNT://$ComputerName/$AdminGroup,group"
+    $UsersGroup.Add($NewUser.path)
+    Write-Host "The user $UserName was added to the group '$AdminGroup'."
+}Catch [System.Management.Automation.MethodInvocationException]{
+    Write-Warning $Error[0].Exception.Message.Split('"')[-2]
+}
+Try{
+    [ADSI]$UsersGroup = "WinNT://$ComputerName/$RDPgroup,group"
+    $UsersGroup.Add($NewUser.path)
+    Write-Host "The user $UserName was added to the group '$RDPgroup'."
+}Catch [System.Management.Automation.MethodInvocationException]{
+    Write-Warning $Error[0].Exception.Message.Split('"')[-2]
+}
