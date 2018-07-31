41ef6ee180670e509c22e909b6d2bf1af99c41ea
ALYSARIL13 <39834053+ALYSARIL13@users.noreply.github.com>
2018-07-31 12:28:23 -0600

    Update WindowsUserManagement.ps1

--- WindowsUserManagement.ps1
+++ WindowsUserManagement.ps1
@@ -1,6 +1,6 @@
 #Setup the variables#
-$UserName = "perg13" #--> It must contains 4 or more characters #
-$Password = 'Hadmin13Hadmin13' #--> It must contains 12 or more characters #
+$UserName = "UserName" #--> It must contains 4 or more characters #
+$Password = 'P@$$w0rd1234' #--> It must contains 12 or more characters #
 
 #Creating the user account
 $ComputerName = $env:COMPUTERNAME
