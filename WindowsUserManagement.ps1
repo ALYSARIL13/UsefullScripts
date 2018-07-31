3a75dff23a1e59606dfdc9e2299cb94f1fdb556a
ALYSARIL13 <39834053+ALYSARIL13@users.noreply.github.com>
2018-07-31 12:21:49 -0600

    Update WindowsUserManagement.ps1

--- WindowsUserManagement.ps1
+++ WindowsUserManagement.ps1
@@ -1,13 +1,13 @@
 #Setup the variables#
-$UserName = "username"
-$Password = 'P@ssw0rd1234' #--> It must contains 12 or more characters #
- 
-#Create the user account
+$UserName = "perg13" #--> It must contains 4 or more characters #
+$Password = 'Hadmin13Hadmin13' #--> It must contains 12 or more characters #
+
+#Creating the user account
 $ComputerName = $env:COMPUTERNAME
 [ADSI]$ADSIcomp = "WinNT://$ComputerName"
 $NewUser = $ADSIcomp.Create("User","$UserName")
- 
-#Set password on the account 
+
+#Setting up password on the user account 
 Try{
     $NewUser.SetPassword("$Password")
     $NewUser.SetInfo()
@@ -23,21 +23,27 @@ Try{
         Write-Warning $Error[0]
     }
 }
- 
-#Add the account to the Groups: "Administartors" & "Remote Desktop Users"
+
+#User Groups
 $AdminGroup = "Administrators"
 $RDPgroup = "Remote Desktop Users"
+
+#Adding the account to the Group:"Administartors"
 Try{
     [ADSI]$UsersGroup = "WinNT://$ComputerName/$AdminGroup,group"
     $UsersGroup.Add($NewUser.path)
     Write-Host "The user $UserName was added to the group '$AdminGroup'."
 }Catch [System.Management.Automation.MethodInvocationException]{
-    Write-Warning $Error[0].Exception.Message.Split('"')[-2]
+    $text = $Error[0].Exception.Message.Split('"')[-2].Split(".")[0]
+    Write-Warning "$text '$AdminGroup'."
 }
+
+#Adding the account to the Group:"Remote Desktop Users"
 Try{
     [ADSI]$UsersGroup = "WinNT://$ComputerName/$RDPgroup,group"
     $UsersGroup.Add($NewUser.path)
     Write-Host "The user $UserName was added to the group '$RDPgroup'."
 }Catch [System.Management.Automation.MethodInvocationException]{
-    Write-Warning $Error[0].Exception.Message.Split('"')[-2]
+    $text = $Error[0].Exception.Message.Split('"')[-2].Split(".")[0]
+    Write-Warning "$text '$RDPgroup'."
 }
