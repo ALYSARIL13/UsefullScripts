2b30e044b92c19ed5ebdf160c7985b361c9ac114
ALYSARIL13 <39834053+ALYSARIL13@users.noreply.github.com>
2018-09-07 18:31:33 +0000

    Update FixFolderACls.ps1

--- FixFolderACls.ps1
+++ FixFolderACls.ps1
@@ -1,48 +1,28 @@
 #====================> FixMachineKeysV5.ps1 <====================#
- 
 #=====>Machine Keys path
 $Folder = "C:\ProgramData\Microsoft\Crypto\RSA\MachineKeys"
- 
+#=====>Users and Groups
+$Owner = New-Object System.Security.Principal.NTAccount("NT AUTHORITY", "SYSTEM")
 #=====>Access Rule set
 $SYSTEM = New-Object System.Security.AccessControl.FileSystemAccessRule("SYSTEM","FullControl","Allow") 
 $Everyone = New-Object System.Security.AccessControl.FileSystemAccessRule("Everyone","Write,Read,Synchronize","Allow")
 $Administrators = New-Object System.Security.AccessControl.FileSystemAccessRule("Administrators","FullControl","Allow")
 $NETWORK_SERVICE = New-Object System.Security.AccessControl.FileSystemAccessRule("NETWORK SERVICE","Write,Read,Synchronize","Allow") 
- 
 #=====>Change the owner to "BUILTIN/Administrators, in order to grant permissions#
 TAKEOWN /A /F $Folder /R /D Y
- 
 #=====>Repairing MachineKeys folder permissions
 $FolderAcl = Get-Acl $Folder
-While($FolderAcl.Access -ne $null){
-    $FolderAcl.Access | %{$FolderAcl.RemoveAccessRule($_)} | Out-Null
-    $FolderAcl | %{$FolderAcl.SetAccessRuleProtection($true,$true)}
-    Set-Acl $Folder $FolderAcl -Verbose
-    $FolderAcl = Get-Acl $Folder
-}
+$FolderAcl.SetOwner($Owner)
 $FolderAcl.AddAccessRule($Administrators), $FolderAcl.AddAccessRule($Everyone)
 Set-Acl $Folder $FolderAcl -Verbose
- 
 #=====>Repairing Keys permissions
 $Keys = Get-ChildItem $Folder -Force
 ForEach($Key in $Keys){
     $KeyAcl = Get-Acl $Key.FullName
-    While($KeyAcl.Access -ne $null){
-        $KeyAcl.Access | %{$KeyAcl.RemoveAccessRule($_)} | Out-Null
-        $KeyAcl | %{$KeyAcl.SetAccessRuleProtection($true,$true)}
-        Set-Acl $Key.FullName $KeyAcl
-        $KeyAcl = Get-Acl $Key.FullName
-    }
-    $KeyAcl.AddAccessRule($SYSTEM), $KeyAcl.AddAccessRule($Administrators), $KeyAcl.AddAccessRule($NETWORK_SERVICE)
+    $KeyAcl.SetOwner($Owner)
+    $KeyAcl.AddAccessRule($SYSTEM), $KeyAcl.AddAccessRule($Administrators), $KeyAcl.AddAccessRule($NETWORK_SERVICE), $KeyAcl.AddAccessRule($Everyone)
     Set-Acl $Key.FullName $KeyAcl -Verbose
 }
- 
-ICACLS $Folder /setowner SYSTEM /T
- 
-#=====>Showing results of execution
-$Output = @()
-$Output = $Output + (Get-ACL $Folder)
-ForEach($Key in $Keys){
-    $Output = $Output + (Get-ACL $Key.FullName)
-}
-$Output | Out-GridView
+#=====>Restart TermService#
+Stop-Service TermService –Force -Verbose
+Start-Service TermService -Verbose
